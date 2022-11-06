cb1d14aeefcf8fa744ce3b597a920c861e4879c4
Adrian Radores <redperses143fps@gmail.com>
2022-11-06 12:54:47 +0800

    add

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,9 +5,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  router: {
-    base: '/portfolio/'
-  },
+
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
