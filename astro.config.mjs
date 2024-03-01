6615fa24ba68218a49fb030f7e3e95525edfef41
Adrian Radores <aradores@chanzclobal.com>
2024-03-01 20:46:42 +0800

    initial

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap(), react({
+      include: ['**/react/*'],
+    })],
+});
